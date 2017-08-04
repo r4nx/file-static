2175b790c3f69f8c62de19cafcd5154bb83626e6
Ranx <mod34@yandex.ru>
2017-08-05 00:01:00 +0300

    Initial commit

---
+++ README.md
@@ -0,0 +1,12 @@
+What is File Static?
+====================
+File Static is a very small and simply python script, that doing some actions when some events are triggered. For example you can configure script to remove certain file if it was created every second.
+
+---
+
+Configuration
+=============
+Option           | Type   | Description
+-----------------|--------|------------
+files            | string | files for processing (can be separated by space)
+additional files | string | additional file(s), using for _modify_ event and _copy_ action
---
+++ filestatic.py
@@ -0,0 +1,134 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+
+import sys
+import os
+import configparser
+from time import sleep
+import filecmp
+import shutil
+import shlex
+try:
+    # Win32
+    from msvcrt import getch
+except ImportError:
+    # UNIX
+    def getch():
+        import tty
+        import termios
+        fd = sys.stdin.fileno()
+        old = termios.tcgetattr(fd)
+        try:
+            tty.setraw(fd)
+            return sys.stdin.read(1)
+        finally:
+            termios.tcsetattr(fd, termios.TCSADRAIN, old)
+
+
+CONFIG_FILE_NAME = 'filestatic.ini'
+
+def main():
+
+    # Config loading
+    config = configparser.RawConfigParser(allow_no_value=True)
+    if os.path.isfile(CONFIG_FILE_NAME):
+        config.read(CONFIG_FILE_NAME)
+    else:
+        # Default config
+        config['Detection'] = {'file': '',
+                               'additional_file': '',
+                               'event_type': '',
+                               'action': '',
+                               'interval': '',
+                               'loop': ''}
+
+        with open(CONFIG_FILE_NAME, 'w') as f:
+            config.write(f)
+
+        print('Configuration file was created! Configure "%s" and run the program again!' % CONFIG_FILE_NAME)
+        any_key()
+
+    files = shlex.split(config.get('Detection', 'files'))
+    additional_files = shlex.split(config.get('Detection', 'additional_files'))
+    event_type = config.get('Detection', 'event_type')
+    action = config.get('Detection', 'action')
+    interval = config.getfloat('Detection', 'interval')
+    loop = config.getboolean('Detection', 'loop')
+    is_looping = True
+
+    if event_type == 'modify':
+        while is_looping:
+            for file in files:
+                additional_file = additional_files[files.index(file)] if len(files) == len(additional_files) \
+                    else additional_files[0] if len(additional_files) > 0 else ''
+                if not filecmp.cmp(file, additional_file):
+                    perform_action(action, file, additional_file)
+                    if not loop:
+                        is_looping = False
+            sleep(interval)
+
+    elif event_type == 'create':
+        while is_looping:
+            for file in files:
+                additional_file = additional_files[files.index(file)] if len(files) == len(additional_files) \
+                    else additional_files[0] if len(additional_files) > 0 else ''
+                if os.path.isfile(file):
+                    perform_action(action, file, additional_file)
+                    if not loop:
+                        is_looping = False
+            sleep(interval)
+
+    elif event_type == 'remove':
+        while is_looping:
+            for file in files:
+                additional_file = additional_files[files.index(file)] if len(files) == len(additional_files) \
+                    else additional_files[0] if len(additional_files) > 0 else ''
+                if not os.path.isfile(file):
+                    perform_action(action, file, additional_file)
+                    if not loop:
+                        is_looping = False
+            sleep(interval)
+
+    elif event_type == 'modify/remove':
+        while is_looping:
+            for file in files:
+                additional_file = additional_files[files.index(file)] if len(files) == len(additional_files) \
+                    else additional_files[0] if len(additional_files) > 0 else ''
+                if not os.path.isfile(file) or not filecmp.cmp(file, additional_file):
+                    perform_action(action, file, additional_file)
+                    if not loop:
+                        is_looping = False
+
+    else:
+        print('Unknown event type: %s' % event_type)
+        any_key()
+
+
+def exception_handler(exception_type, exception, traceback):
+    print('An error occurred during program execution!')
+    print('\nError information:')
+    print('  Error type: %s' % exception_type)
+    print('  Error message: %s' % (exception.message if hasattr(exception, 'message') else str(exception)))
+    any_key()
+
+
+def perform_action(action, file, additional_file):
+    if action == 'copy':
+        shutil.copy(additional_file, file)
+    elif action == 'remove':
+        os.remove(file)
+    else:
+        print('Unknown action: "%s"' % action)
+    print('Action performed!')
+
+
+def any_key():
+    print('\nPress any key to exit...')
+    getch()
+    sys.exit()
+
+
+sys.excepthook = exception_handler
+
+if __name__ == '__main__':
+    main()
