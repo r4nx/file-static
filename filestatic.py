81ec1f1feebfb43962ed9c2333b50525832d0aea
Ranx <mod34@yandex.ru>
2017-08-05 08:38:42 +0300

    Added license, minor improvements

--- filestatic.py
+++ filestatic.py
@@ -1,6 +1,22 @@
 #!/usr/bin/python
 # -*- coding: utf-8 -*-
 
+# <one line to give the program's name and a brief idea of what it does.>
+# Copyright (C) <year>  <name of author>
+
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
 import sys
 import os
 import configparser
@@ -59,7 +75,7 @@ def main():
     if event_type == 'modify':
         while is_looping:
             for file in files:
-                additional_file = additional_files[files.index(file)] if len(files) == len(additional_files) \
+                additional_file = additional_files[files.index(file)] if len(files) <= len(additional_files) \
                     else additional_files[0] if len(additional_files) > 0 else ''
                 if not filecmp.cmp(file, additional_file):
                     perform_action(action, file, additional_file)
@@ -70,7 +86,7 @@ def main():
     elif event_type == 'create':
         while is_looping:
             for file in files:
-                additional_file = additional_files[files.index(file)] if len(files) == len(additional_files) \
+                additional_file = additional_files[files.index(file)] if len(files) <= len(additional_files) \
                     else additional_files[0] if len(additional_files) > 0 else ''
                 if os.path.isfile(file):
                     perform_action(action, file, additional_file)
@@ -81,7 +97,7 @@ def main():
     elif event_type == 'remove':
         while is_looping:
             for file in files:
-                additional_file = additional_files[files.index(file)] if len(files) == len(additional_files) \
+                additional_file = additional_files[files.index(file)] if len(files) <= len(additional_files) \
                     else additional_files[0] if len(additional_files) > 0 else ''
                 if not os.path.isfile(file):
                     perform_action(action, file, additional_file)
@@ -92,7 +108,7 @@ def main():
     elif event_type == 'modify/remove':
         while is_looping:
             for file in files:
-                additional_file = additional_files[files.index(file)] if len(files) == len(additional_files) \
+                additional_file = additional_files[files.index(file)] if len(files) <= len(additional_files) \
                     else additional_files[0] if len(additional_files) > 0 else ''
                 if not os.path.isfile(file) or not filecmp.cmp(file, additional_file):
                     perform_action(action, file, additional_file)
